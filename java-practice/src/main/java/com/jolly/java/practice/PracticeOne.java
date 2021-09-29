b8900f3452fb67e382f99f9626852c565c437265
Raman Ojha <ojha.raman99@gmail.com>
2021-09-29 09:33:28 +0530

    added mul function

--- java-practice/src/main/java/com/jolly/java/practice/PracticeOne.java
+++ java-practice/src/main/java/com/jolly/java/practice/PracticeOne.java
@@ -4,6 +4,7 @@ public class PracticeOne {
 	public static void main (String[]args){
 		PracticeOne.add(10, 20);
 		PracticeOne.sub(30, 21);
+		PracticeOne.mul(10, 15);
 	}
 	public static void add(int a , int b){
 		System.out.println(a+b);
@@ -11,5 +12,8 @@ public class PracticeOne {
 	}
 	public static void sub(int a , int b){
 		System.out.println(a-b);
+	}
+	public static void mul (int a ,int b){
+		System.out.println(a*b);
 	}
 }
