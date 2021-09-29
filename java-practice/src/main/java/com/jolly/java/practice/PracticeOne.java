1a79efa099054c9ee0a620ff0b506eaedde8199a
Raman Ojha <ojha.raman99@gmail.com>
2021-09-29 09:17:49 +0530

    added sub function

--- java-practice/src/main/java/com/jolly/java/practice/PracticeOne.java
+++ java-practice/src/main/java/com/jolly/java/practice/PracticeOne.java
@@ -3,9 +3,13 @@ package com.jolly.java.practice;
 public class PracticeOne {
 	public static void main (String[]args){
 		PracticeOne.add(10, 20);
+		PracticeOne.sub(30, 21);
 	}
 	public static void add(int a , int b){
 		System.out.println(a+b);
 		
+	}
+	public static void sub(int a , int b){
+		System.out.println(a-b);
 	}
 }
