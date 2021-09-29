92c558cc02a0d798be94f0f265cdcbc2612a9db3
Raman Ojha <ojha.raman99@gmail.com>
2021-09-29 08:58:28 +0530

    Added Sum Function

--- java-practice/src/main/java/com/jolly/java/practice/PracticeOne.java
+++ java-practice/src/main/java/com/jolly/java/practice/PracticeOne.java
@@ -1,5 +1,11 @@
 package com.jolly.java.practice;
 
 public class PracticeOne {
-
+	public static void main (String[]args){
+		PracticeOne.add(10, 20);
+	}
+	public static void add(int a , int b){
+		System.out.println(a+b);
+		
+	}
 }
